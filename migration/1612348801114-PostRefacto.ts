cd9d6f09ab25ef88e280018df0d81b40bf499ed7
mamun <abdullah.ssc13@gmail.com>
2021-02-03 18:11:33 +0600

    vul val

---
+++ migration/1612348801114-PostRefacto.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class PostRefacto1612348801114 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {}
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,29 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { EventsModule } from './events/events.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
-        MongooseModule.forRoot('mongodb://localhost/exonchat'),
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: '1230',
+            database: 'exonchat',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            migrationsTableName: 'migrations',
+            migrations: ['migration/*.ts'],
+            cli: {
+                migrationsDir: 'migration',
+            },
+            synchronize: true,
+        }),
+        // MongooseModule.forRoot('mongodb://localhost/exonchat'),
         EventsModule,
     ],
     controllers: [AppController],
