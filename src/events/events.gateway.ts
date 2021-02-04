6f4f2ca1e930f3a4c2cd8bc0822602bf55d7eca8
Mohammed Younus <younusdev@gmail.com>
2021-02-04 18:33:43 +0600

    add events

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -13,7 +13,7 @@ import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({ transports: ['websocket'], serveClient: false })
 export class EventsGateway
     implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer()
@@ -21,6 +21,9 @@ export class EventsGateway
 
     // server is eq to socket.io's io
     // client|Socket is eq to socket.io's socket
+
+    private clientsToRoom: any = {};
+    private apiToAgents: any = {};
 
     @SubscribeMessage('agents-online')
     async agentsOnline(@MessageBody() data: any): Promise<number> {
@@ -34,6 +37,34 @@ export class EventsGateway
         @ConnectedSocket() client: Socket,
     ): Promise<number> {
         // this.server.emit('msg to client');
+        console.log(data);
+
+        return;
+    }
+
+    @SubscribeMessage('webchat-new-message')
+    async whebchatNewMsg(
+        @MessageBody() data: any,
+        @ConnectedSocket() client: Socket,
+    ): Promise<number> {
+        this.server
+            .in(this.clientsToRoom[client.id])
+            .emit('message', { msg: data.msg, sentByClient: true }); // send to room also with sender
+
+        // loop through all client.id present in this.apiToAgents[client.handshake.query.api]
+        // this.server.to(client.id).emit('new-message-to-agents', { data });
+
+        return;
+    }
+
+    @SubscribeMessage('pong')
+    async pong(
+        @MessageBody() data: number,
+        @ConnectedSocket() client: Socket,
+    ): Promise<number> {
+        // this.server.emit('msg to client');
+        console.log(data);
+
         return data;
     }
 
@@ -41,11 +72,19 @@ export class EventsGateway
         console.log('Init');
     }
 
-    handleDisconnect(client: Socket) {
-        console.log(`Client disconnected: ${client.id}`);
-    }
-
     handleConnection(client: Socket, ...args: any[]) {
         console.log(`Client connected: ${client.id}`);
+
+        // if client
+        // check the token from client.handshake.query.token and get assigned room which is conversation_id
+        // let roomName = room_name
+        // this.client.join(roomName)
+
+        //if agent
+        // save to this.apiToAgents = client.id
+    }
+
+    handleDisconnect(client: Socket) {
+        console.log(`Client disconnected: ${client.id}`);
     }
 }
