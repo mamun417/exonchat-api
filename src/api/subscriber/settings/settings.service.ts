dc2c20599ac5fd3340dbc34326ceb24e85924d22
mamun417 <abdullah.ssc13@gmail.com>
2021-05-27 18:16:08 +0600

    few change

--- src/api/subscriber/settings/settings.service.ts
+++ src/api/subscriber/settings/settings.service.ts
@@ -1,33 +1,24 @@
 import { HttpException, HttpService, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-
 import { intent } from '@prisma/client';
 import { DataHelper } from 'src/helper/data-helper';
 import { UpdateUiSettingsDto } from './dto/update-ui-settings.dto';
-
 import * as _l from 'lodash';
 import { UpdateAppSettingsDto } from './dto/update-app-settings.dto';
-
 @Injectable()
 export class SettingsService {
     constructor(private prisma: PrismaService, private dataHelper: DataHelper) {}
-
     async updateUISetting(req: any, updateUISettingsDto: UpdateUiSettingsDto) {
         if (!updateUISettingsDto.ui_settings.length)
             throw new HttpException(`UI Settings can not be empty`, HttpStatus.BAD_REQUEST);
-
         const adminUser = await this.prisma.user.findFirst({
             where: { id: req.user.data.id, role: { slug: 'admin' } },
         });
-
         const settings = [];
-
         for (const setting of updateUISettingsDto.ui_settings) {
             if (!_l.isPlainObject(setting) || !setting.name || !setting.value)
                 throw new HttpException(`UI Settings update structure not good`, HttpStatus.BAD_REQUEST);
-
             const temp: any = await this.findOneWithException(setting.name, req);
-
             if (
                 temp.category !== 'ui' ||
                 temp.user_type === 'administrator' ||
@@ -38,38 +29,32 @@ export class SettingsService {
                     `You dont have permission to change some of the settings`,
                     HttpStatus.FORBIDDEN,
                 );
-
             settings.push({
                 ...setting,
                 from_db: temp,
             });
         }
-
         for (const setting of settings) {
             const updateOrCreate: any = {};
-
-            if (setting.user_settings_value.length) {
+            if (setting.from_db.user_settings_value.length) {
                 updateOrCreate.update = {
-                    where: { id: setting.user_settings_value[0].id },
+                    where: { id: setting.from_db.user_settings_value[0].id },
                     data: { value: setting.value },
                 };
             } else {
-                updateOrCreate.create = [{ value: setting.value }];
+                updateOrCreate.create = { value: setting.value, subscriber_id: req.user.data.subscriber_id };
             }
-
             if (setting.from_db.user_type === 'user') {
                 // its dynamic but for eye not good
                 // updateOrCreate[Object.keys(updateOrCreate)[0]][
                 //     Object.keys(updateOrCreate)[0] === 'update' ? 'update' : 0
                 // ].user_id = req.user.data.id;
-
-                if (setting.user_settings_value.length) {
+                if (setting.from_db.user_settings_value.length) {
                     updateOrCreate.update.data.user_id = req.user.data.id;
                 } else {
-                    updateOrCreate.create[0].user_id = req.user.data.id;
+                    updateOrCreate.create.user_id = req.user.data.id;
                 }
             }
-
             if (setting.from_db.user_type)
                 await this.prisma.setting.update({
                     where: {
@@ -86,10 +71,8 @@ export class SettingsService {
                     },
                 });
         }
-
         return this.getUISettings(req);
     }
-
     async getUISettings(req: any) {
         return this.prisma.setting.findMany({
             where: {
@@ -106,44 +89,39 @@ export class SettingsService {
             },
         });
     }
-
     async updateAppSetting(req: any, updateAppSettingsDto: UpdateAppSettingsDto) {
         if (!updateAppSettingsDto.app_settings.length)
             throw new HttpException(`App Settings can not be empty`, HttpStatus.BAD_REQUEST);
-
         const adminUser = await this.prisma.user.findFirst({
             where: { id: req.user.data.id, role: { slug: 'admin' } },
         });
-
         if (!adminUser)
             throw new HttpException(`You dont have permission to change some of the settings`, HttpStatus.FORBIDDEN);
-
         const settings = [];
-
         for (const setting of updateAppSettingsDto.app_settings) {
             if (!_l.isPlainObject(setting) || !setting.name || !setting.value)
                 throw new HttpException(`UI Settings update structure not good`, HttpStatus.BAD_REQUEST);
-
             const temp: any = await this.findOneWithException(setting.name, req);
-
-            if (
-                temp.category !== 'app' ||
-                temp.user_type !== 'subscriber' ||
-                !temp.subscriber_id ||
-                temp.subscriber_id !== req.user.data.subscriber_id
-            )
+            if (temp.category !== 'app' || temp.user_type !== 'subscriber' || temp.subscriber_id)
                 throw new HttpException(
                     `You dont have permission to change some of the settings`,
                     HttpStatus.FORBIDDEN,
                 );
-
             settings.push({
                 ...setting,
                 from_db: temp,
             });
         }
-
         for (const setting of settings) {
+            const updateOrCreate: any = {};
+            if (setting.from_db.user_settings_value.length) {
+                updateOrCreate.update = {
+                    where: { id: setting.from_db.user_settings_value[0].id },
+                    data: { value: setting.value },
+                };
+            } else {
+                updateOrCreate.create = { value: setting.value, subscriber_id: req.user.data.subscriber_id };
+            }
             if (setting.from_db.user_type)
                 await this.prisma.setting.update({
                     where: {
@@ -154,24 +132,31 @@ export class SettingsService {
                         },
                     },
                     data: {
-                        default_value: setting.value,
+                        user_settings_value: {
+                            ...updateOrCreate,
+                        },
                     },
                 });
         }
-
         return this.getAppSettings(req);
     }
-
     async getAppSettings(req: any) {
+        // if no user_settings_value then use the default_value from root
         return this.prisma.setting.findMany({
             where: {
                 category: 'app',
                 user_type: 'subscriber',
-                subscriber_id: req.user.data.subscriber_id,
+                subscriber_id: null,
+            },
+            include: {
+                user_settings_value: {
+                    where: {
+                        subscriber_id: req.user.data.subscriber_id,
+                    },
+                },
             },
         });
     }
-
     async findOne(slug: string, req: any) {
         return this.prisma.setting.findFirst({
             where: {
@@ -187,11 +172,9 @@ export class SettingsService {
             },
         });
     }
-
     async findOneWithException(slug: string, req: any): Promise<intent> {
         return this.dataHelper.getSingleDataWithException(async () => await this.findOne(slug, req), 'settings');
     }
-
     async findOneWithoutUserSetting(slug: string, req: any) {
         return this.prisma.setting.findFirst({
             where: {
@@ -199,7 +182,6 @@ export class SettingsService {
             },
         });
     }
-
     async findOneWithoutUserSettingWithException(slug: string, req: any): Promise<intent> {
         return this.dataHelper.getSingleDataWithException(
             async () => await this.findOneWithoutUserSetting(slug, req),
