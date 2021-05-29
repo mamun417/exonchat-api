934420cc64f406cd11f46111cfba4d2bf17661ff
mamun417 <abdullah.ssc13@gmail.com>
2021-05-29 18:25:46 +0600

    whmcs api connect, validate login and get tickets api created

--- src/api/subscriber/apps/third-party/whmcs/whmcs.controller.ts
+++ src/api/subscriber/apps/third-party/whmcs/whmcs.controller.ts
@@ -1,3 +1,4 @@
+import { ValidateUserDto } from './dto/ValidateUser.dto';
 import { Controller, Request, Get, Post, Body, Put, Param, Delete, UseGuards } from '@nestjs/common';
 import { WHMCSService } from './whmcs.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
@@ -10,5 +11,17 @@ export class WHMCSController {
     @Post()
     findAll(@Request() req: any) {
         return this.WHMCSService.findAll(req);
+    }
+
+    // @UseGuards(JwtAuthGuard)
+    @Post('users/validate')
+    validateUser(@Request() req: any, @Body() validateUserDto: ValidateUserDto) {
+        return this.WHMCSService.validateUser(req, validateUserDto);
+    }
+
+    // @UseGuards(JwtAuthGuard)
+    @Get('user/ticketes/:client_id')
+    findTickets(@Param('client_id') client_id: string, @Request() req: any) {
+        return this.WHMCSService.findTickets(req, client_id);
     }
 }
--- src/api/subscriber/apps/third-party/whmcs/whmcs.service.ts
+++ src/api/subscriber/apps/third-party/whmcs/whmcs.service.ts
@@ -1,3 +1,4 @@
+import { ValidateUserDto } from './dto/ValidateUser.dto';
 import { HttpException, HttpService, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
@@ -7,5 +8,43 @@ export class WHMCSService {
 
     async findAll(req: any) {
         return { note: 'call whmcs api for ticket get' };
+    }
+
+    async validateUser(req: any, validateUserDto: ValidateUserDto) {
+        const params = {
+            action: 'ValidateLogin',
+            email: validateUserDto.email,
+            password2: validateUserDto.password,
+        };
+
+        return await this.getResponse(params);
+    }
+
+    async findTickets(req: any, clientId: any) {
+        const params = {
+            action: 'GetTickets',
+            clientid: clientId,
+        };
+
+        return await this.getResponse(params);
+    }
+
+    async getResponse(dynamicFields: any) {
+        const params = new URLSearchParams({
+            username: '3tuMBl8jtZ6xYgiZDUqfiHpFuroPu0Ch',
+            password: 'mp9FzClGBjvdEUzaBz3wBg9IlIHuwSwW',
+            responsetype: 'json',
+            ...dynamicFields,
+        });
+
+        try {
+            const res: any = await this.httpService
+                .post('https://dev.exonhost.com/includes/api.php', params.toString())
+                .toPromise();
+
+            return res.data;
+        } catch (e) {
+            return e.response.data;
+        }
     }
 }
