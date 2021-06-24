7a4fb8296e28550aa9b920e66a9c894b5b85a45b
Mohammed Younus <younusdev@gmail.com>
2021-06-24 15:17:37 +0600

    refactor partial seed upsert so that we don't need to reset database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import * as _f from 'faker';
 import * as _l from 'lodash';
-import { create } from 'node:domain';
 
 const prisma = new PrismaClient();
 
@@ -40,12 +38,41 @@ async function main() {
 
     const rolesData = await Promise.all(
         roles.map(async (role) => {
-            return await prisma.role.create({
-                data: {
+            return await prisma.role.upsert({
+                where: {
+                    role_identifier: {
+                        slug: role.slug,
+                        use_for: role.use_for,
+                    },
+                },
+                create: {
                     slug: role.slug,
                     name: role.name,
                     use_for: role.use_for,
-                    permissions: { create: role.permissions },
+                    permissions: {
+                        connectOrCreate: role.permissions.map((permission: any) => {
+                            return {
+                                where: {
+                                    permission_identifier: { slug: permission.slug, use_for: permission.use_for },
+                                },
+                                create: { slug: permission.slug, use_for: permission.use_for, name: permission.name },
+                            };
+                        }),
+                    },
+                },
+                update: {
+                    name: role.name,
+                    use_for: role.use_for,
+                    permissions: {
+                        connectOrCreate: role.permissions.map((permission: any) => {
+                            return {
+                                where: {
+                                    permission_identifier: { slug: permission.slug, use_for: permission.use_for },
+                                },
+                                create: { slug: permission.slug, use_for: permission.use_for, name: permission.name },
+                            };
+                        }),
+                    },
                 },
             });
         }),
@@ -185,70 +212,104 @@ async function main() {
         });
     }
 
-    await prisma.setting.createMany({
-        data: [
-            {
-                slug: 'global_color',
-                display_name: 'Global Color',
-                category: 'ui',
-                default_value: 'green',
-                user_type: 'user',
-            },
+    const settingsData = [
+        {
+            slug: 'global_color',
+            display_name: 'Global Color',
+            category: 'ui',
+            default_value: 'green',
+            user_type: 'user',
+        },
 
-            {
-                slug: 'apps_whmcs_identifier_key',
-                display_name: 'Identifier Key',
-                category: 'app',
-                sub_category: 'whmcs',
-                default_value: '',
-                user_type: 'subscriber',
+        {
+            slug: 'apps_whmcs_identifier_key',
+            display_name: 'Identifier Key',
+            category: 'app',
+            sub_category: 'whmcs',
+            default_value: '',
+            user_type: 'subscriber',
+        },
+        {
+            slug: 'apps_whmcs_secret_key',
+            display_name: 'Secret Key',
+            category: 'app',
+            sub_category: 'whmcs',
+            default_value: '',
+            user_type: 'subscriber',
+        },
+        {
+            slug: 'apps_whmcs_enable',
+            display_name: 'Connection Enable/Disable',
+            category: 'app',
+            sub_category: 'whmcs',
+            default_value: 'false',
+            user_type: 'subscriber',
+            input_type: 'checkbox',
+        },
+        {
+            slug: 'apps_whmcs_ticket_notification',
+            display_name: 'Ticket notification',
+            category: 'app',
+            sub_category: 'whmcs',
+            default_value: 'false',
+            user_type: 'subscriber',
+            input_type: 'checkbox',
+        },
+        {
+            slug: 'apps_whmcs_ticket_manage',
+            display_name: 'Ticket manager',
+            category: 'app',
+            sub_category: 'whmcs',
+            default_value: 'false',
+            user_type: 'subscriber',
+            input_type: 'checkbox',
+        },
+        {
+            slug: 'apps_whmcs_ticket_submit_from_chat',
+            display_name: 'Ticket submit from chat',
+            category: 'app',
+            sub_category: 'whmcs',
+            default_value: 'false',
+            user_type: 'subscriber',
+            input_type: 'checkbox',
+        },
+
+        {
+            slug: 'ai_auto_reply_at_client_msg',
+            display_name: 'Ticket submit from chat',
+            category: 'ai',
+            sub_category: 'reply',
+            default_value: 'false',
+            user_type: 'subscriber',
+            input_type: 'checkbox',
+        },
+    ];
+
+    for (const setting of settingsData) {
+        const category: any = setting.category;
+        const user_type: any = setting.user_type;
+        const input_type: any = setting.input_type || 'text';
+
+        await prisma.setting.upsert({
+            where: {
+                slug_identifier: {
+                    slug: setting.slug,
+                    category: category,
+                    user_type: user_type,
+                },
             },
-            {
-                slug: 'apps_whmcs_secret_key',
-                display_name: 'Secret Key',
-                category: 'app',
-                sub_category: 'whmcs',
-                default_value: '',
-                user_type: 'subscriber',
+            create: {
+                slug: setting.slug,
+                display_name: setting.display_name,
+                category: category,
+                sub_category: setting.sub_category,
+                default_value: setting.default_value,
+                user_type: user_type,
+                input_type: input_type,
             },
-            {
-                slug: 'apps_whmcs_enable',
-                display_name: 'Connection Enable/Disable',
-                category: 'app',
-                sub_category: 'whmcs',
-                default_value: 'false',
-                user_type: 'subscriber',
-                input_type: 'checkbox',
-            },
-            {
-                slug: 'apps_whmcs_ticket_notification',
-                display_name: 'Ticket notification',
-                category: 'app',
-                sub_category: 'whmcs',
-                default_value: 'false',
-                user_type: 'subscriber',
-                input_type: 'checkbox',
-            },
-            {
-                slug: 'apps_whmcs_ticket_manage',
-                display_name: 'Ticket manager',
-                category: 'app',
-                sub_category: 'whmcs',
-                default_value: 'false',
-                user_type: 'subscriber',
-                input_type: 'checkbox',
-            },
-            {
-                slug: 'apps_whmcs_ticket_submit_from_chat',
-                display_name: 'Ticket submit from chat',
-                category: 'app',
-                sub_category: 'whmcs',
-                default_value: 'false',
-                user_type: 'subscriber',
-                input_type: 'checkbox',
-            },
-        ],
-    });
+            update: {},
+        });
+    }
 }
 
 main()
